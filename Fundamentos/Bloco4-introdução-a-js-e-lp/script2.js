de8f2ca99996268df03cc83ec158c008e32e526d
devgeorge <glalmeida.dev@gmail.com>
2021-07-28 16:01:20 -0300

    Adicionando exercicios

---
+++ Fundamentos/Bloco4-introdução-a-js-e-lp/script2.js
@@ -0,0 +1,19 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientEmail)
+console.log(typeof patientId)
+console.log(typeof isEnrolled)
+console.log(typeof patientInfo)
+
+let base = 5;
+let altura = 8;
+let area = base*altura; 
+console.log(area)
+let perimetro = 26;
+console.log(perimetro)

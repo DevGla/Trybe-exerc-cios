ff932e92cd6f183746c63b2564468806791eebc9
devgeorge <glalmeida.dev@gmail.com>
2021-08-05 18:05:51 -0300

    Adicionando exericicios

---
+++ Fundamentos/Bloco4-introdução-a-js-e-lp/Dia2-array-for/script-array.js
@@ -0,0 +1,11 @@
+/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = 'Serviços';
+console.log(menuServices);*/
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+let indexOfmenu = menu.indexOf('Portfólio');
+console.log(indexOfmenu);
+
+menu.push("contato");
+console.log(menu);

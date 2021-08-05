d8815e25a6ae00415ff6bca5eaee8df044629ce6
devgeorge <glalmeida.dev@gmail.com>
2021-08-05 10:17:56 -0300

    Adicionando exercicios

---
+++ Fundamentos/Bloco4-introdução-a-js-e-lp/script3.js
@@ -0,0 +1,23 @@
+/*let a = 10;
+let b = 15;
+console.log(a+b)
+console.log(a-b)
+console.log(a*a)
+console.log(a/b)
+console.log(a%b)
+
+/*let a = 27;
+let b = 35;
+
+if (a > b){
+    console.log(a)
+} else {
+    console.log(b)
+}*/
+
+/*let a = 25;
+let b = 105;
+let c = 101;*/
+
+
+

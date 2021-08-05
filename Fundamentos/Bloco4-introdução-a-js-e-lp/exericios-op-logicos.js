9f5c752e5862509f660637269fb2e61190ce2dac
devgeorge <glalmeida.dev@gmail.com>
2021-08-05 11:34:15 -0300

    Adicionando exericicos

--- Fundamentos/Bloco4-introdução-a-js-e-lp/exericios-op-logicos.js
+++ Fundamentos/Bloco4-introdução-a-js-e-lp/exericios-op-logicos.js
@@ -1,4 +1,4 @@
-const currentHour = 4;
+/*const currentHour = 4;
 let message = "";
 
 if (currentHour >= 22){
@@ -15,4 +15,28 @@ else if (currentHour >= 11 && currentHour <= 14){
 }
 else if (currentHour >= 4 && currentHour <= 11){
     console.log(message = "Hmmm, cheio de café recém passado!")
+}*/
+
+/*let WeekDay = "Segunda-feira";
+if (WeekDay == "Segunda-feira" || WeekDay =="Terça-feira" || WeekDay =="Quarta-feira" || WeekDay =="Quinta-feira" || WeekDay =="sexta-feira"){
+    console.log("Oba, mais um dia de aprendizado na Trybe")
+} else {
+    console.log("FINALMENTE, descanso merecido UwU")
+}*/
+
+//console.log(!(2 + 2) === 4);
+
+let resultado = "Reprovado"; 
+switch (resultado){
+    case  ("Aprovada"):
+        console.log("Parabéns Você foi Aprovado(a).")
+    break
+    case  ("Lista"):
+        console.log("Você está na lista de espera!")
+    break
+    case  ("Reprovado"):
+        console.log("Infelizmente você não alcançou a pontuação mínima e foi Repovado(a).")
+    break
+    default:
+        console.log("Não se aplica")
 }

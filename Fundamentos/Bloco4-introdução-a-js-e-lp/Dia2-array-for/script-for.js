05ad360b9fbf46eb8d2775877a7dab7393baf804
devgeorge <glalmeida.dev@gmail.com>
2021-08-05 21:53:07 -0300

    Adicionado exercicios

---
+++ Fundamentos/Bloco4-introdução-a-js-e-lp/Dia2-array-for/script-for.js
@@ -0,0 +1,23 @@
+/*let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index < groceryList.length; index += 1){
+    console.log(groceryList[index])
+}*/
+
+/*let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let nomes of names){
+    console.log(nomes)
+}*/
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//for (let numeros of numbers){
+    //console.log(numeros);
+//}
+/*for (index = 0; index < numbers.length; index += 1){
+    let resultado = numbers[index] + ;
+    console.log(resultado);
+}*/
+let soma = 0;
+for(var i = 0; i < numbers.length; i++){
+    soma = soma + numbers[i] / numbers.length;
+    console.log(soma)
+}
+

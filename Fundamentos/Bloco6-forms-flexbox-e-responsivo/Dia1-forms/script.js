0920bfd125f3a5fba0d2134d46e4ad0c0aa96dfb
devgeorge <glalmeida.dev@gmail.com>
2021-08-21 11:37:46 -0300

    Adicionando exercicio introdutorio

---
+++ Fundamentos/Bloco6-forms-flexbox-e-responsivo/Dia1-forms/script.js
@@ -0,0 +1,9 @@
+function CriarEstadoOption(){
+    let estados = document.getElementById("input-estados");
+    let siglasEstados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+    for (index = 0; index < siglasEstados ; index += 1){
+    let criarOpcoes = document.createElement("option");
+    estados.appendChild(criarOpcoes).innerText = siglasEstados[index];
+    estados.appendChild(criarOpcoes).value = siglasEstados[index];
+}
+}

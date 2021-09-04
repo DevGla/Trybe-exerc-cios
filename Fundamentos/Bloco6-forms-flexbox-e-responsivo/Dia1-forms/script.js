94a580895feca079fff5907a143bde19fd81fceb
devgeorge <glalmeida.dev@gmail.com>
2021-09-04 14:37:05 -0300

    Adicionado exericicos

--- Fundamentos/Bloco6-forms-flexbox-e-responsivo/Dia1-forms/script.js
+++ Fundamentos/Bloco6-forms-flexbox-e-responsivo/Dia1-forms/script.js
@@ -1,7 +1,8 @@
+
 function CriarEstadoOption(){
     let estados = document.getElementById("input-estados");
     let siglasEstados = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
-    for (index = 0; index < siglasEstados ; index += 1){
+    for ( let index = 0; index < siglasEstados.length; index += 1){
     let criarOpcoes = document.createElement("option");
     estados.appendChild(criarOpcoes).innerText = siglasEstados[index];
     estados.appendChild(criarOpcoes).value = siglasEstados[index];
